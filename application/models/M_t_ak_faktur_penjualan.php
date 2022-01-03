1cce06d2c3da10c882a25b0b868d81ef4a4e9892
unknown <dermawanantony@gmail.com>
2022-01-03 17:14:22 +0700

    up

--- application/models/M_t_ak_faktur_penjualan.php
+++ application/models/M_t_ak_faktur_penjualan.php
@@ -262,7 +262,7 @@ public function select_by_date_payment_method($date_from,$date_to,$payment_metho
     
 
 
-    $this->db->order_by("NO_FAKTUR", "desc");
+    $this->db->order_by("NO_FAKTUR", "asc");
 
     $akun = $this->db->get ();
     return $akun->result ();
